966d0cf496a992d32774a5bf06df386c587bb4d2
EnzoGD <106977951+EnzoGD@users.noreply.github.com>
2024-06-11 16:20:23 -0300

    uploads

--- js.main.js
+++ js.main.js
@@ -4,10 +4,53 @@ const caixaAlternativas = document.querySelector('.caixa-alternativas');
 const caixaResultado = document.querySelector('.caixa-resultado');
 const textoResultado = document.querySelector('.texto-resultado');
 
-const perguntas= [
+const perguntas = [
     {
-    enunciado: "Pergunta 1",
-    alternativas: [ "Alternativa 1", "Alternativa 2"
-
-    ]
-    }]
+        enunciado: "Qual tecnologia tem potencial para revolucionar o esporte no futuro?",
+        alternativas: [
+            "Realidade aumentada",
+            "Inteligência Artificial",
+            "Nanotecnologia",
+            "Veículos autônomos"
+        ]
+    },
+    {
+        enunciado: "Qual tendência tem impactado o esporte atualmente?",
+        alternativas: [
+            "Esportes eletrônicos(E-sports)",
+            "Treinamento com realidade virtual",
+            "Monitoramento genético de atletas",
+            "Equipamentos biodegradáveis"
+        ]
+    },
+    {
+        enunciado: "Como a prática esportiva pode ser influenciada pela tecnologia no futuro?",
+        alternativas: [
+            "Personalização extrema de treinamentos",
+            "Competições interplanetárias",
+            "Uniformes com capacidade de autorregeneração",
+            "Implantes bioeletrônicos para melhorar o desempenho"
+        ]
+    },
+    {
+        enunciado: "Qual fator terá maior impacto nas competições esportivas no futuro?",
+        alternativas: [
+            "Alternativa 1",
+            "Alternativa 2"
+        ]
+    },
+    {
+        enunciado: "Pergunta 5",
+        alternativas: [
+            "Alternativa 1",
+            "Alternativa 2"
+        ]
+    },
+    {
+        enunciado: "Pergunta 6",
+        alternativas: [
+            "Alternativa 1",
+            "Alternativa 2"
+        ]
+    },
+]; 
